138ae0f49b4a23ad88efdee9cd9664117f265d7a
Nikitha-gr <nikitha14.gr@gmail.com>
2025-01-05 11:49:38 +0530

    Under 100 HTML and CSS

--- Scripts/newsletter.js
+++ Scripts/newsletter.js
@@ -2,7 +2,7 @@ export const newsletter = () => {
     let newsletter = document.getElementById("newsletter");
     let card = ` <div class="newstext">
       <h1>Join Our List, Get 10% Off</h1>
-      <p>Sign up for Seara emails—new subscribers get 10% off a future order!* Plus, get early access to sales, coupons, and more. (One code per email address.)</p>
+      <p>Sign up for Seara emails—new subscribers get 10% off on a future order!* Plus, get early access to sales, coupons, and more. (One code per email address.)</p>
       <form class="newsletter-form">
         <input type="email" placeholder="Enter your email address" class="email-input">
         <button class="submit-button">Join The Party</button>
